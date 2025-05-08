c22ea12b2067c4127eb1425a11b7e4d89eef0532
BrianAb <144299964+BrianAbH@users.noreply.github.com>
2025-05-08 09:58:07 -0500

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,38 @@
+function mostrarValores() {
+  const nombre = document.getElementById('nombre').value;
+  const apellido = document.getElementById('apellido').value;
+  const edad = document.getElementById('edad').value;
+  const correo = document.getElementById('correo').value;
+  const carrera = document.getElementById('carreras').value;
+  const telefono = document.getElementById('telefono').value;
+  const fechaInscripcion = document.getElementById('fecha').value;
+
+  const genero = document.querySelector('input[name="genero"]:checked');
+  
+
+  const redes = [];
+  document.querySelectorAll('input[type="checkbox"]').forEach(checkbox => {
+    if (checkbox.checked) {
+      redes.push(checkbox.value);
+    }
+  });
+
+  const generoValor = genero ? genero.value : alert("Seleccione un genero");
+
+  const resultado = `    
+  Nombre: ${nombre}
+  Apellido: ${apellido}
+  Edad: ${edad}
+  Correo Electronico: ${correo}
+  Carrera: ${carrera}
+  Telefono: ${telefono}
+  Fecha de Inscripción: ${fechaInscripcion}
+  Género: ${generoValor}
+  Visto por: ${redes.join(', ') || 'Ninguno'}
+    `;
+  if(nombre.length && apellido.length && edad.length && correo.length && carrera.length && telefono.length && fechaInscripcion.length> 0){  
+    alert(resultado.trim());
+  }
+
+
+}
